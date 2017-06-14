df6ddce9ef34fb53c1788693f64c041ff482ccc7
Shivani Bimavarapu <sbima001@odu.edu>
2017-06-14 15:51:34 -0400

    home component edited
    home component and styles.scss edited

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../post.service';
 import { Post } from '../post';
+import { routerTransition } from '../animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [routerTransition],
+  host: {'[@routerTransition]': ''}
 })
 export class HomeComponent implements OnInit {
 
