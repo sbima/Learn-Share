d701693a5b9e8b051320ab5be4c7e1e6d1f07df6
Shivani Bimavarapu <sbima001@odu.edu>
2017-06-14 15:44:42 -0400

    api.js edited
    api.js edited

--- server/routes/api.js
+++ server/routes/api.js
@@ -25,5 +25,31 @@ router.get('/tutorials', function(req, res) {
         });
 });
 
+router.get('/details/:id', function(req, res) {
+    console.log('Requesting post');
+    post.findById(req.params.id)
+        .exec(function(err, post) {
+            if (err) {
+                console.log('Error getting the post');
+            } else {
+                res.json(post);
+            }
+        });
+});
+
+router.post('/tutorials', function(req, res) {
+    console.log('Posting a post');
+    var newPost = new post();
+    newPost.title = req.body.title;
+    newPost.url = req.body.url;
+    newPost.description = req.body.description;
+    newPost.save(function(err, addedPost) {
+        if (err) {
+            console.log('Error inserting the post');
+        } else {
+            res.json(addedPost);
+        }
+    });
+});
 
 module.exports = router;
